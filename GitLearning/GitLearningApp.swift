c5fa2662135525bbb3afc6aa1909939c5351f0b2
brett696 <brettcsmith@gmail.com>
2025-05-26 15:34:53 -0400

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Club Smith on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
