be224fa3a993f39694e4a7c51203cba036e5c6f2
brett696 <brettcsmith@gmail.com>
2025-05-26 20:27:43 -0400

    Introduced a sheet to display DetailView

--- GitLearning/ContentView.swift
+++ GitLearning/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
   
+  @State var showSheet = false
+  
     var body: some View {
       NavigationStack {
         VStack {
@@ -17,9 +19,16 @@ struct ContentView: View {
           }, label: {
             Text("Git Learning App")
             })
-
+          Button(action: {
+            showSheet = true
+          }, label: {
+            Text("Show Sheet")
+          })
           }
         .padding()
+        .sheet(isPresented: $showSheet, content: {
+          DetailView()
+        })
       }
     }
 }
