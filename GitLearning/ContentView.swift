21e0d5238e552a4b673f8789a920c7e3512b9bc2
brett696 <brettcsmith@gmail.com>
2025-05-27 12:43:37 -0400

    Refactored NavigationLink to navigationDestination

--- GitLearning/ContentView.swift
+++ GitLearning/ContentView.swift
@@ -10,15 +10,16 @@ import SwiftUI
 struct ContentView: View {
   
   @State var showSheet = false
+  @State var showNavigation = false
   
     var body: some View {
       NavigationStack {
         VStack {
-          NavigationLink(destination: {
-            DetailView()
+          Button(action: {
+            showNavigation = true
           }, label: {
             Text("Git Learning App")
-            })
+          })
           Button(action: {
             showSheet = true
           }, label: {
@@ -29,6 +30,9 @@ struct ContentView: View {
         .sheet(isPresented: $showSheet, content: {
           DetailView()
         })
+        .navigationDestination(isPresented: $showNavigation) {
+          DetailView()
+        }
       }
     }
 }
